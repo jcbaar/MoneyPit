a36316b7e05f3a5f6d457e8a898b558ba003e913
Jan van den Baard <jcbaar@outlook.com>
2014-12-30 18:27:23 +0100

    - The android generateViewId() is now called if the device is running API level 17 or higher. Othwerwise the buildt in version is used.

--- app/src/main/java/com/development/jaba/utilities/UtilsHelper.java
+++ app/src/main/java/com/development/jaba/utilities/UtilsHelper.java
@@ -1,5 +1,10 @@
 package com.development.jaba.utilities;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.test.suitebuilder.annotation.Suppress;
+import android.view.View;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -15,14 +20,22 @@ public class UtilsHelper {
      *
      * @return a generated ID value
      */
+    @SuppressLint("NewApi")
     public static int generateViewId() {
-        for (;;) {
-            final int result = sNextGeneratedId.get();
-            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
-            int newValue = result + 1;
-            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
-            if (sNextGeneratedId.compareAndSet(result, newValue)) {
-                return result;
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            for (;;) {
+                final int result = sNextGeneratedId.get();
+                // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+                int newValue = result + 1;
+                if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+                if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                    return result;
+                }
             }
         }
-    }}
+        else {
+            return View.generateViewId();
+        }
+    }
+}
