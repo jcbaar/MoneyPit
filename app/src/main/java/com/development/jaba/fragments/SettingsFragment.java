34b963c155ef06c25c0f3cd12b0881b92c6784b9
Jan van den Baard <jcbaar@outlook.com>
2015-01-23 14:12:52 +0100

    - Changed some strings. - Added an option to clear the local map cache from the preferences screen.

--- app/src/main/java/com/development/jaba/fragments/SettingsFragment.java
+++ app/src/main/java/com/development/jaba/fragments/SettingsFragment.java
@@ -1,9 +1,16 @@
 package com.development.jaba.fragments;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.development.jaba.moneypit.R;
+import com.development.jaba.utilities.DialogHelper;
+
+import java.io.File;
 
 /**
  * A simple fragment for editing the application settings.
@@ -33,5 +40,36 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        Preference pref = findPreference("emptyMapCache");
+        if (pref != null) {
+            pref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    DialogHelper.showYesNoDialog(getString(R.string.warning), getString(R.string.empty_cache), new MaterialDialog.ButtonCallback() {
+                                @Override
+                                public void onPositive(MaterialDialog dialog) {
+                                    super.onPositive(dialog);
+
+                                    File cache = new File(Environment.getExternalStorageDirectory() + "/MoneyPit/mapcache/");
+                                    String[] files = cache.list();
+                                    boolean ok = true;
+                                    for (String f : files) {
+                                        File file = new File(cache.getAbsolutePath() + "/" + f);
+                                        ok &= file.delete();
+                                    }
+
+                                    if (ok) {
+                                        Toast.makeText(getActivity(), getString(R.string.cache_purged), Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(getActivity(), getString(R.string.cache_purge_failed), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            },
+                            getActivity());
+                    return true;
+                }
+            });
+        }
     }
 }
