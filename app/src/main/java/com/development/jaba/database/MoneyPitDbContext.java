6ba6d3ebeba7d94d8631d0c11b7205374e9e00cf
Jan van den Baard <jcbaar@outlook.com>
2015-01-27 11:21:22 +0100

    - Bug fix: Cascading deletes were not working because the foreign key support has to be activated explicitly which is wasn't.

--- app/src/main/java/com/development/jaba/database/MoneyPitDbContext.java
+++ app/src/main/java/com/development/jaba/database/MoneyPitDbContext.java
@@ -57,6 +57,24 @@ public class MoneyPitDbContext extends SQLiteOpenHelper {
     }
     //endregion
 
+
+    //region Overrides
+
+    /**
+     * Make sure foreign key constraints are active.
+     *
+     * @param db The {@link android.database.sqlite.SQLiteDatabase}.
+     */
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        if (!db.isReadOnly()) {
+            // Enable foreign key constraints
+            db.execSQL("PRAGMA foreign_keys=ON;");
+        }
+    }
+    //endregion
+
     //region Database create/update
     @Override
     public void onCreate(SQLiteDatabase db) {
